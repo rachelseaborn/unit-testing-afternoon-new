345085fa81eb82e1e1af3b2a7a7b4bc67c02612e
rachelseaborn <raseaborn@gmail.com>
2021-01-21 13:21:42 -0700

    buggy

--- server/utils.js
+++ server/utils.js
@@ -5,16 +5,30 @@ module.exports = {
       0,
     );
   },
+  //   attachUserName(users, posts) {
+  //     let userDict = users.reduce((accum, user) => {
+  //       accum[user.id] = user;
+  //       return accum;
+  //     }, {});
+  //     return posts.map(post => {
+  //       post.displayName = `${userDict[post.userId].first} ${
+  //         userDict[post.userId].last
+  //       }`;
+  //       return post;
+  //     });
+  //   },
+  // };
+
   attachUserName(users, posts) {
     let userDict = users.reduce((accum, user) => {
       accum[user.id] = user;
       return accum;
     }, {});
-    return posts.map(post => {
-      post.displayName = `${userDict[post.userId].first} ${
-        userDict[post.userId].last
-      }`;
-      return post;
-    });
-  },
-};
+    return posts
+      .filter(post => userDict[post.userId])
+      .map(post => {
+        post.displayName = `${userDict[post.userId].first} ${userDict[post.userID].last}`;
+        return post;
+      });
+  }
+}
